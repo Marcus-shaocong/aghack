cf67c306b93a3bc4d3826a9823468b1a482710ae
Yufan Lou <loganlyf@gmail.com>
2019-06-15 14:12:00 +0800

    Add some sprites

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
 function setup() {
   // put setup code here
+  createCanvas(800, 300);
+
+  let table = createSprite(90, height/2, 100, 100);
+  let bins = new Group();
+  for (let i = 0; i < 4; i++) {
+    let bin = createSprite(300 + i * 120, 200, 100, 100);
+    bins.add(bin);
+  }
+
+  let piece = createSprite(90, height/2, 50, 50);
+
 }
 
 function draw() {
   // put drawing code here
+  background(255, 255, 255);
+
+  drawSprites();
 }
